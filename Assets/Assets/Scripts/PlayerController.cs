096bd120818230660a8708a16ff20646393adaf2
Brando123412 <130480267+Brando123412@users.noreply.github.com>
2023-05-15 20:40:40 -0500

    Tarea6Finihs

--- Assets/Assets/Scripts/PlayerController.cs
+++ Assets/Assets/Scripts/PlayerController.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
+using UnityEngine.EventSystems;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D myRBD2;//Para su mismo rigidbody
@@ -17,6 +18,10 @@ public class PlayerController : MonoBehaviour
     [SerializeField] Soundscriptableobjects SoundDamage;
     [SerializeField] AudioSource lowLifeSound;
     [SerializeField] HealthBarController lowLife;
+
+    Vector2 movementPlayer;
+    Vector3 mouseInput;
+    Vector3 distance;
     private void Start() {
         GetComponent<HealthBarController>().onHit += cameraReference.CallScreenShake;/*
         Siempre y cuando el personaje reciba daño la camara se movera
@@ -26,21 +31,21 @@ public class PlayerController : MonoBehaviour
 
     private void Update() {
         //Para mover el personaje en el eje X y Y
-        Vector2 movementPlayer = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        //Vector2 movementPlayer = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         //Aqui se esta multiplicando el movimiento generado por el player la velocidad modificada
         myRBD2.velocity = movementPlayer * velocityModifier;
         //Llama a la animacion de moverse cuando el personaje se mueve
         animatorController.SetVelocity(velocityCharacter: myRBD2.velocity.magnitude);
         //Este vector3 se crea para guarda la posicion del mouse en tiempo actual
-        Vector3 mouseInput = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //Vector3 mouseInput = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         //Este metodo llamado es para que el personaje siempre mire a la posicion del mouse
         CheckFlip(mouseInput.x);
         //Este vector3 es para hallar la distancia entre el personaje en tiempo real
-        Vector3 distance = mouseInput - transform.position;
+        distance = mouseInput - transform.position;
         //Esto dibuja el posible rayo que se genera con el calculo que se hizo en la parte superio
         Debug.DrawRay(transform.position, distance * rayDistance, Color.red);
 
-        if(Input.GetMouseButtonDown(0)){
+        /*if(Input.GetMouseButtonDown(0)){
             //Si hacemos click entonces vamos a instanciar una bala, todo dependiendo de  la posicion del player
             BulletController myBullet =  Instantiate(bulletPrefab, transform.position, Quaternion.identity);
             //Al momento de generar la bala vamos a pasar parametros para que la bala salga en la direccion correcta y al mismo tiempo se le da el tag de player
@@ -48,7 +53,7 @@ public class PlayerController : MonoBehaviour
             myBullet.SetUpVelocity(distance.normalized, gameObject.tag,SoundBala);
         }else if(Input.GetMouseButtonDown(1)){
             Debug.Log("Left Click");
-        }
+        }*/
         if(lowLife.currentValue<25){
             lowLifeSound.Play();
         }
@@ -56,5 +61,25 @@ public class PlayerController : MonoBehaviour
     //Esto simplemente hace que se cambie la direccion de mirar 
     private void CheckFlip(float x_Position){
         spriteRenderer.flipX = (x_Position - transform.position.x) < 0;
+    }
+
+    public void OnMovement(InputAction.CallbackContext value)
+    {
+        Vector2 inputMovement = value.ReadValue<Vector2>();
+        movementPlayer = new Vector2(inputMovement.x, inputMovement.y);
+    }
+    public void OnAim(InputAction.CallbackContext value)
+    {
+        Vector2 inputMouse = Camera.main.ScreenToWorldPoint(value.ReadValue<Vector2>());
+        mouseInput = inputMouse;
+    }
+    public void OnFire(InputAction.CallbackContext value)
+    {
+        if (value.started) {
+            BulletController myBullet =  Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+            myBullet.SetUpVelocity(distance.normalized, gameObject.tag,SoundBala);
+            /*Vector2 disparo = new Vector2((mouseInput.x - transform.position.x), (mouseInput.y - transform.position.y));
+            Instantiate(bulletPrefab, disparo, Quaternion.identity);*/
+        }
     }
 }
